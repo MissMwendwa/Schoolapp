064d0534085eec3f57885b366d92956eb42e8ccf
MissMwendwa <wanjamwesh@gmail.com>
2024-02-21 13:19:36 +0300

    Views Update V1

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
 # Create your views here.
 def loginuser(request):
     if request.method == 'POST':
         user = authenticate(
             request, username = request.POST['username'], password =request.POST['password'])
         if user is None:
-            message.error(
+            messages.error(
                 request, 'username and Password did not Match. Please Try again')
             return render(request, 'login.html')
         else:
@@ -14,3 +17,9 @@ def loginuser(request):
             return redirect('homepage')
     else:
         return render(request, 'login.html')
+
+
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect('loginuser')
